af25e4f010ac48307c986e4e8a67505b9b1af47f
Linda-neves <neveslinda637@gmail.com>
2025-12-09 16:53:51 +0200

    Added API integration and text aligned the poem

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
+function displayPoem(response)
+{
+    new Typewriter("#poem", {
+       strings: response.data.answer,
+       autoStart: true,
+       delay: 1,
+       cursor: "",
+    });
+}
+
 function generatePoem(event)
 {
    event.preventDefault();
 
-   new Typewriter("#poem", {
-       strings: ["Here comes <br />the best poem"],
-       autoStart: true,
-       delay: 1,
+   let instructionInput = document.querySelector("#user-instructions");
+   let apiKey = "bc0cf33438a473et001fo0823a9ca9ff";
+   let prompt = `User instructions: Generate a poem ${instructionInput.value}`;
+   let context = "You are a romatic poet. Write a short poem that has a bold heading centered in the middle and separate each line with a <br />. Make sure to follow the user instructions.";
+   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    });
-
-
+   axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator");
